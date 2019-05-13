c459391e55838e069d5522466e33430158572e32
Jenny Shaw <bloo.j.shaw@gmail.com>
2019-05-13 01:20:33 +0000

    Automatically backed up by Learn

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -22,7 +22,7 @@ def play(songs)
   puts "Please enter a song name or number:"
   user_choice = gets.chomp
   songs.each_with_index do |song, index|
-    if user_choice == song || user_choice == index-1
+    if song.include?(user_choice) || user_choice == index-1
       "Playing #{song}"
     else
       "Invalid input, please try again"
